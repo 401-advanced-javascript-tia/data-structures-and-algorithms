7f32f461b3eb2d3ea348ecc1fd14203e9c658ff3
Tia Low <lowtia@gmail.com>
2020-08-14 20:35:16 -0700

    Work in progress, need to fix toString and work on tests

--- javascript/data-structures/linked-list/linked-list.js
+++ javascript/data-structures/linked-list/linked-list.js
@@ -46,35 +46,47 @@ class LinkedList {
     return false;
   }
 
-  append(value){
-    // add to the tail (aka end) of the list
-
-    const newNode = new Node(value);
-
-    if (!this.head) {
-      this.head = new Node(value);
-      return;
-    }
-
-    // while loop looking for a node with a next of null
+  toString() {
 
     let currentNode = this.head;
 
-    while(currentNode.next) {
-      // while currentNode.next is truthy which means it !== null
+    let resultArr = [];
+
+    while(currentNode !== null) {
+
+      resultArr.push({currentNode});
+
       currentNode = currentNode.next;
+
     }
-    //we now know that currentNode is a node, in fact its the last node
 
-    currentNode.next = newNode;
+    let arrToStr = resultArr.join(' -> ');
 
-
+    return arrToStr;
   }
 
+  //  ----------------- APPEND METHOD -----------------
+  // append(value){
+  //   // add to the tail (aka end) of the list
+  //   const newNode = new Node(value);
+  //   if (!this.head) {
+  //     this.head = new Node(value);
+  //     return;
+  //   }
+  //   // while loop looking for a node with a next of null
+  //   let currentNode = this.head;
+  //   while(currentNode.next) {
+  //     // while currentNode.next is truthy which means it !== null
+  //     currentNode = currentNode.next;
+  //   }
+  //   //we now know that currentNode is a node, in fact its the last node
+  //   currentNode.next = newNode;
+  // }
+  // ---------------------------------------------------
+
+  // another thing you can do is make an array with the values that are in the linked list. traverse the list like in append() above, push each value to an array
+
+
 }
-
-
-// another thing you can do is make an array with the values that are in the linked list. traverse the list like in append() above, push each value to an array
-
 
 module.exports = LinkedList;
