e0b78be1f629546716adb1d2e79380d1d93dc563
Tia Low <lowtia@gmail.com>
2020-08-25 07:48:49 -0700

    Wrote tests for queue with stacks

--- javascript/challenges/__tests__/queue-with-stacks.test.js
+++ javascript/challenges/__tests__/queue-with-stacks.test.js
@@ -10,14 +10,45 @@ describe('PseudoQueue class', () => {
 
   it('should enqueue to empty stack', () => {
 
-    const pseudoqueue = new PseudoQueue();
-    pseudoqueue.enqueue('thing1');
-    expect(pseudoqueue.length).toBe(1);
+    const pqueue = new PseudoQueue();
+    pqueue.enqueue('thing1');
+    expect(pqueue.length).toBe(1);
   });
 
+  it('should enqueue multiple items', () => {
+    const pqueue = new PseudoQueue();
+    pqueue.enqueue('thing1');
+    pqueue.enqueue('thing2');
+    pqueue.enqueue('thing3');
+    expect(pqueue.length).toBe(3);
+  });
 
+  it('should properly dequeue', () => {
+    const pqueue = new PseudoQueue();
+    pqueue.enqueue('thing1');
+    pqueue.enqueue('thing2');
+    pqueue.enqueue('thing3');
+    expect(pqueue.length).toBe(3);
 
+    expect(pqueue.dequeue()).toBe('thing3');
+    expect(pqueue.length).toBe(2);
 
+  });
 
+  it('should throw error when dequeueing from already empty stack', () => {
+    const pqueue = new PseudoQueue();
+    expect(() => pqueue.dequeue()).toThrow(RangeError);
+  });
+
+  it('should enqueue and dequeue multiple times', () => {
+    const pqueue = new PseudoQueue();
+    pqueue.enqueue('thing1');
+    expect(pqueue.dequeue()).toBe('thing1');
+    pqueue.enqueue('thing2');
+    expect(pqueue.dequeue()).toBe('thing2');
+    pqueue.enqueue('thing3');
+    expect(pqueue.dequeue()).toBe('thing3');
+    expect(() => pqueue.dequeue()).toThrow(RangeError);
+  });
 
 });
--- javascript/challenges/queuesWithStacks/queue-with-stacks.js
+++ javascript/challenges/queuesWithStacks/queue-with-stacks.js
@@ -7,32 +7,33 @@ class PseudoQueue {
   constructor () {
     this.stack1 = new Stack();
     this.stack2 = new Stack();
+    this.length = 0;
   }
 
   enqueue(value){
-    //adds a new node with the value to the back of the queue with O(1) time performance
-
     this.stack1.push(value);
-
+    this.length++;
   }
 
 
   dequeue(){
 
-    if(this.stack1.length === 0){
+    if(this.stack1.isEmpty()){
       throw new RangeError('Cannot dequeue from empty stack.');
     } else {
-      while(this.stack1.length > 0){
+      while(!this.stack1.isEmpty()){
         let poppedItem = this.stack1.pop();
+        this.length--;
         this.stack2.push(poppedItem);
       }
     }
 
     const valOfPopped = this.stack2.pop();
 
-    while(this.stack2.length > 0){
+    while(!this.stack2.isEmpty()){
       let itemPopped = this.stack2.pop();
       this.stack1.push(itemPopped);
+      this.length++;
     }
 
     return valOfPopped;
