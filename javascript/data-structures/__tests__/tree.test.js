db107ecfda1669cf9e7ca0d29f64fd274d991364
Tia Low <lowtia@gmail.com>
2020-08-28 19:07:18 -0700

    Solution code and passing tests for binary tree traverse methods

--- javascript/data-structures/__tests__/tree.test.js
+++ javascript/data-structures/__tests__/tree.test.js
@@ -1,8 +1,101 @@
 'use strict';
 
-// Can successfully instantiate an empty tree
-// Can successfully instantiate a tree with a single root node
-// Can successfully add a left child and right child to a single root node
-// Can successfully return a collection from a preorder traversal
-// Can successfully return a collection from an inorder traversal
-// Can successfully return a collection from a postorder traversal
+const { BinaryTree } = require('../tree/tree.js');
+const { BinarySearchTree } = require('../tree/tree.js');
+const { Node } = require('../tree/tree.js');
+
+
+describe('BinaryTree Class', () => {
+
+  it('should be a class', () => {
+    expect(BinaryTree).toBeDefined();
+  });
+
+  it('should instantiate an empty tree', () => {
+    const newTree = new BinaryTree();
+    expect(newTree.root).toBe(null);
+  });
+
+  it('should instantiate a new tree with a single root node', () => {
+    const newTree = new BinaryTree(new Node('redwood'));
+    expect(newTree.root.value).toBe('redwood');
+  });
+
+  it('should add a left child and a right child to a single root node', () => {
+    const cedarNode = new Node('cedar');
+    const mapleNode = new Node('maple');
+    const birchNode = new Node('birch', mapleNode, cedarNode);
+    const newTree = new BinaryTree(birchNode);
+    expect(newTree.root.value).toBe('birch');
+    expect(newTree.root.left.value).toBe('maple');
+    expect(newTree.root.right.value).toBe('cedar');
+  });
+
+  describe('preOrder traversal', () => {
+
+    it('should return correct collection from preOrder traversal', () => {
+      const birchNode = new Node('birch');
+      const beechNode = new Node('beech');
+      const alderNode = new Node('alder', birchNode, beechNode);
+      const mapleNode = new Node('maple');
+      const cedarNode = new Node('cedar', alderNode, mapleNode);
+      const newTree = new BinaryTree(cedarNode);
+      expect(newTree.root.value).toBe('cedar');
+      expect(newTree.root.left.left.value).toBe('birch');
+      expect(newTree.preOrder()).toEqual(['cedar', 'alder', 'birch', 'beech', 'maple']);
+    });
+
+  });
+
+  describe('inOrder traversal', () => {
+
+    it('should return correct collection from inOrder traversal', () => {
+      const birchNode = new Node('birch');
+      const beechNode = new Node('beech');
+      const alderNode = new Node('alder', birchNode, beechNode);
+      const spruceNode = new Node('spruce');
+      const mapleNode = new Node('maple', spruceNode);
+      const cedarNode = new Node('cedar', alderNode, mapleNode);
+      const newTree = new BinaryTree(cedarNode);
+      expect(newTree.root.value).toBe('cedar');
+      expect(newTree.root.left.right.value).toBe('beech');
+      expect(newTree.root.right.left.value).toBe('spruce');
+      expect(newTree.inOrder()).toEqual(['birch', 'alder', 'beech', 'cedar', 'spruce', 'maple']);
+    });
+
+  });
+
+  describe('postOrder traversal', () => {
+
+    it('should return correct collection from postOrder traversal', () => {
+  
+      const birchNode = new Node('birch');
+      const beechNode = new Node('beech');
+      const alderNode = new Node('alder', birchNode, beechNode);
+      const spruceNode = new Node('spruce');
+      const mapleNode = new Node('maple', spruceNode);
+      const cedarNode = new Node('cedar', alderNode, mapleNode);
+      const newTree = new BinaryTree(cedarNode);
+      expect(newTree.postOrder()).toEqual(['birch', 'beech', 'alder', 'spruce', 'maple', 'cedar']);
+  
+    });
+
+  })
+
+
+});
+
+
+
+
+// NOT REQUIRED- tests for add and contains
+
+
+describe('BinarySearchTree Class', () => {
+
+  it('should be a class', () => {
+    expect(BinarySearchTree).toBeDefined();
+  });
+
+});
+
--- javascript/data-structures/tree/tree.js
+++ javascript/data-structures/tree/tree.js
@@ -1,34 +1,5 @@
 'use strict';
 
-
-// const output = [];
-
-// function preOrder(root) {
-//   // add root's value to an array
-//   // check for null
-
-//   if(!root) {
-//     return;
-//     // throw new Error('No root');
-//   }
-
-//   output.push(root.value);
-
-//   preOrder(root.left);
-//   // this involves one extra function call but it works because it root is null it will just return out with the if statement above
-//   preOrder(root.right);
-
-
-//   // if(root.left) {
-//   //   preOrder(root.left);
-//   // }
-
-//   // if(root.right) {
-//   //   preOrder(root.right);
-//   // }
-
-// }
-
 class BinaryTree {
 
   constructor( root = null) {
@@ -55,6 +26,48 @@ class BinaryTree {
     return arr;
 
   }
+
+  inOrder() {
+
+    const arr = [];
+
+    function _inOrder(root) {
+
+      if(!root){
+        return;
+      }
+
+      _inOrder(root.left);
+      arr.push(root.value);
+      _inOrder(root.right);
+    }
+
+    _inOrder(this.root);
+    return arr;
+  }
+
+  postOrder() {
+
+    const arr = [];
+
+    function _postOrder(root) {
+
+      if(!root) {
+        return;
+      }
+
+      _postOrder(root.left);
+      _postOrder(root.right);
+      arr.push(root.value);
+
+    }
+
+    _postOrder(this.root);
+    return arr;
+  }
+
+
+
 }
 
 class Node {
@@ -67,19 +80,73 @@ class Node {
 
 }
 
-const bananaNode = new Node('bananas');
-const cucumberNode = new Node('cucumbers');
-const appleNode = new Node('apples', bananaNode, cucumberNode);
 
-const tree = new BinaryTree(appleNode);
-const resultsPreOrder = tree.preOrder();
-const resultsInOrder = tree.inOrder();
-const resultsPostOrder = tree.postOrder();
+class BinarySearchTree extends BinaryTree {
 
-console.log(resultsPreOrder);
-console.log(resultsInOrder);
-console.log(resultsPostOrder);
+  add(value) {
+    //adds a new Node with that value in the correct location in the search tree
+
+    // create a Node with the value so we have something to put somewhere 
+    // first check if the tree is empty, if so just add the Node
+    //if not, we need to enter tree and check if root.value is > or < than value passed in
+    // if smaller, go left and check again
+    // if bigger, go right and check again
+    // go until the node we're at has no left or right, and put it there
+    // at the node we're on, we'll make it the left or right
+
+    let newNode = new Node(value);
+
+    if(!this.root) {
+      this.root = newNode;
+    }
+
+    let currentNode = this.root;
+
+    function _add(currentNode, newNode) {
+
+      if(newNode.value < currentNode.value) {
+
+        if(currentNode.left === null) {
+          currentNode.left = newNode;
+        } else {
+          _add(currentNode.left, newNode);
+        }
+
+      }
+    }
+
+    _add(currentNode, newNode);
 
 
 
+
+
+  }
+
+  contains(value) {
+    // returns a boolean indicating whther or not the value is in the tree at least once
+
+    if (!this.root) {
+      return false;
+    }
+
+    if (this.root.value === value) {
+      return true;
+    }
+
+    if (value < this.root.value) {
+      // GO LEFT
+    } else if (value > this.root.value) {
+      // GO RIGHT
+    }
+
+
+
+  }
+}
+
+
+
+
+module.exports = {BinaryTree, BinarySearchTree, Node};
 
