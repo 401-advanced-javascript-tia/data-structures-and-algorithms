3efb93e9cbb48178d18ea5a15efbdd9492086aba
Tia Low <lowtia@gmail.com>
2020-09-01 14:49:58 -0700

    Updated readme, wrote solution code, need to test next

---
+++ javascript/challenges/__tests__/fizzbuzz.test.js
@@ -0,0 +1,27 @@
+' use strict';
+
+const fizzBuzzTree = require('../fizzBuzzTree/fizz-buzz-tree.js');
+const Node = require('../../data-structures/tree/tree.js');
+const BinaryTree = require('../../data-structures/tree/tree.js');
+
+describe('Fizz Buzz Tree', () => {
+
+  it('should throw error when list is empty', () => {
+
+
+  });
+
+  it('should return proper fizz buzz tree with tree with one node', () => {
+
+
+  });
+
+  it('should return proper fizz buzz tree with multiple nodes', () => {
+
+
+  });
+
+
+
+});
+
