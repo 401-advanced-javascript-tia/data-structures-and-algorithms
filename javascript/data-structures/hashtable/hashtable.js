31a6f9d7f5c4fc8236f851e85d95f3707861526b
Tia Low <lowtia@gmail.com>
2020-09-19 12:50:56 -0700

    Cleaned up console logs

--- javascript/data-structures/hashtable/hashtable.js
+++ javascript/data-structures/hashtable/hashtable.js
@@ -14,16 +14,12 @@ class Hashtable {
     // SIGNATURE: takes in a key and value, doesn't return anything
 
     const index = this.hash(key);
-
     const contentsOfBucket = this.buckets[index];
-
-    // console.log('content in bucket for collission test:', contentsOfBucket);
 
     if ( contentsOfBucket === undefined ){
       this.buckets[index] = [ [key, value] ];
     } else {
       this.buckets[index].push( [key, value] );
-      // console.log('got here in collission case');
     }
 
   }
@@ -33,33 +29,16 @@ class Hashtable {
 
     // SIGNATURE: takes in a key which is (probably) a string, return the value from the table that corresponds to that key, that value could be any data type, it would be whatever type it was when it was added (which is called "type any")
 
-    // console.log('++++++ KEY:', key);
-    
     const index = this.hash(key);
 
-    // console.log('++++++ INDEX:', index);
-
     const itemsInBucket = this.buckets[index];
-    console.log('++++++ ITEMSINBUCKET:', itemsInBucket);
 
     if(itemsInBucket === undefined) {
       return null;
     } else {
 
-      // V CURIOUS WHY THE BELOW DOESNT WORK!
-
-      // itemsInBucket.forEach(item => {
-      //   if(item[0] === key){
-      //     return item[1];
-      //   } else {
-      //     return null;
-      //   }
-      // });
-
-    
       for(let item of itemsInBucket) {
 
-        console.log('+++++ITEM in ITEMSINBUCKET:', item);
         if(item[0] === key){
           return item[1];
         }
