b58f3c55c26f0178fa78d840f3364ce26b28c4b8
Tia Low <lowtia@gmail.com>
2020-09-28 10:36:16 -0700

    Few notes from lecture

--- javascript/data-structures/graphs/graphs.js
+++ javascript/data-structures/graphs/graphs.js
@@ -94,6 +94,27 @@ class Graph {
   //   // return list visual in string form
   // }
 
+  // breadthFirstTraversal
+  // !!make sure to mark the root as visited as well!!!
+  // keep track of visited nodes in a set, and then check (line 111) to see if child is in visited list. if so, dont pay any attention to it. if not, do the stuff to it
+
+  // ALGORITHM BreadthFirst(vertex)
+  //   DECLARE nodes <-- new Set()
+  //   DECLARE breadth <-- new Queue()
+  //   breadth.Enqueue(vertex)
+
+  //   while (breadth is not empty)
+  //       DECLARE front <-- breadth.Dequeue()
+  //       nodes.Add(front)
+
+  //       for each child in front.Children
+  //           if(child is not visited)
+  //               child.Visited <-- true OR create new visited set (set of key value pairs where the value doesnt matter) and check against that ( O(1) search time)
+  // or you need to set .visited property back to false
+  //               breadth.Enqueue(child)
+
+  //   return nodes;
+
 
 }
 
