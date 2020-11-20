c27ff0845395c7a5e55b1f3bbe43d82d14149bf4
Tia Low <lowtia@gmail.com>
2020-11-19 21:07:03 -0800

    Free code camp javascript cert challenge re nesting for loops

--- free-code-camp-challenges/jsCertification.js
+++ free-code-camp-challenges/jsCertification.js
@@ -11,4 +11,21 @@ for (var i = 0; i < myArr.length; i++){
     total = total += myArr[i];
 }
 
+// Nesting for loops
+
+function multiplyAll(arr) {
+    var product = 1;
+// Only change code below this line
+
+    for( let i = 0; i < arr.length; i++ ){
+        for ( let j = 0; j < arr[i].length; j++ ){
+        product = product * arr[i][j];
+    }
+}
+
+// Only change code above this line
+return product;
+}
+
+multiplyAll([[1,2],[3,4],[5,6,7]]);
 
