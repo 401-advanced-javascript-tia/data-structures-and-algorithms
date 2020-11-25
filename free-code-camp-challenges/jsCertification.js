281d60385ab7433f06b8fe24996b8a0aebaeebc8
Tia Low <lowtia@gmail.com>
2020-11-24 20:52:26 -0800

    Working through JS tutorial on Free Code Camp, compound assignment with augmented multiplication

--- free-code-camp-challenges/jsCertification.js
+++ free-code-camp-challenges/jsCertification.js
@@ -70,3 +70,13 @@ a -= 6;
 b -= 15;
 c -= 1;
 
+// -------------------- Compound Assignment with Augmented Multiplation
+
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
