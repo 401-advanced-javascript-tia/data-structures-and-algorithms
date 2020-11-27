6a737f6922fb48c3c0ef6d59812fe05e275eb1dc
Tia Low <lowtia@gmail.com>
2020-11-26 21:32:41 -0800

    Javascript tutorials on Free Code Camp, use bracket notation to find first character in a string

--- free-code-camp-challenges/jsCertification.js
+++ free-code-camp-challenges/jsCertification.js
@@ -132,6 +132,14 @@ var lastName = "Lovelace";
 
 lastNameLength = lastName.length;
 
+// -------------------- Bracket notation to find character in string
 
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
 
 
