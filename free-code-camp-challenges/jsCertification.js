a73b3da0e59c2a85fbe81d1dda7007c4f7599c5e
Tia Low <lowtia@gmail.com>
2020-11-28 22:35:47 -0800

    Javascript curriculum from Free Code Camp, assignment with a returned value

--- free-code-camp-challenges/jsCertification.js
+++ free-code-camp-challenges/jsCertification.js
@@ -178,3 +178,17 @@ function addFive() {
 
 addThree();
 addFive();
+
+// -------------------- Assignment with a returned value
+
+
+// Setup
+var processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+
+processed = processArg(7);
