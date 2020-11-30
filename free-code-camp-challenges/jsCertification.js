9fe19c9b8415f6e4ef5bbcbf0f435fddba406daf
Tia Low <lowtia@gmail.com>
2020-11-30 11:25:12 -0800

    Javascript curriculum through Free Code Camp, stand in line queue

--- free-code-camp-challenges/jsCertification.js
+++ free-code-camp-challenges/jsCertification.js
@@ -192,3 +192,27 @@ function processArg(num) {
 // Only change code below this line
 
 processed = processArg(7);
+
+
+// -------------------- Stand in line (queue)
+
+
+function nextInLine(arr, item) {
+  // Only change code below this line
+
+  arr.push(item);
+  let itemToReturn = arr[0];
+  arr.splice(0, 1)
+  
+  return itemToReturn;
+  // Only change code above this line
+
+}
+
+// Setup
+var testArr = [1,2,3,4,5];
+
+// Display code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
