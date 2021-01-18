a7519b54d704640e9dde478939e20f3d9e94f6c1
Tia Low <lowtia@gmail.com>
2021-01-18 11:23:48 -0800

    Addtl passing test for merge sort, merge already sorted arr

--- javascript/challenges/mergeSort2/__tests__/mergeSort2.test.js
+++ javascript/challenges/mergeSort2/__tests__/mergeSort2.test.js
@@ -4,6 +4,7 @@ const mergeSort = require('../merge-sort2.js');
 
 
 describe('test suite for merge sort', () => {
+
   it('should properly merge unsorted arr', () => {
 
     const unsortedArr = [3, 1, 8, 9, 2, 4, 1];
@@ -24,6 +25,13 @@ describe('test suite for merge sort', () => {
 
     const unsortedArr= [11, 34, 21, 98, 33, 874, 642, 12, 8, 71];
     const sortedArr= [8, 11, 12, 21, 33, 34, 71, 98, 642, 874];
+    expect(mergeSort(unsortedArr)).toEqual(sortedArr);
+  });
+
+  it('should properly merge already sorted array', () => {
+
+    const unsortedArr = [1, 2, 3, 6, 9, 10, 23, 99, 121, 197];
+    const sortedArr = [1, 2, 3, 6, 9, 10, 23, 99, 121, 197];
     expect(mergeSort(unsortedArr)).toEqual(sortedArr);
   });
 
